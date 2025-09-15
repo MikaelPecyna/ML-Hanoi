7fc9b4413ffd62961efcf1935166d68074c85472
mpecyna <mpecyna@ubuntu>
2025-09-15 14:22:28 +0200

    Initialisation du jeu de base

---
+++ JeuHanoi.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class Jeu_Hanoi: 
+    def __init__(self): 
+        self.pic = np.zeros([3,3],dtype=np.int)
+        self.nombre_palet = np.zeros(3, dtype=np.int)
+        pass
+
---
+++ main.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+from JeuHanoi import Jeu_Hanoi
+
+jeu = Jeu_Hanoi() 
+
+#init de la partie au début
+jeu.nombre_palet[0] = 3
+jeu.pic[0,0] = 3
+jeu.pic[0,1] = 2
+jeu.pic[0,2] = 1
+
+#init de la partie finale voulu 
+jeu_finale = Jeu_Hanoi()
+
+Jeu_final.pic[2,0] = 3
+Jeu_final.pic[2,1] = 2
+Jeu_final.pic[2,2] = 1
+
+
+
