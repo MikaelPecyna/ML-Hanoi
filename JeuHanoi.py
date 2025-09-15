2ba1d382e93581ca8400409382db362915659403
mpecyna <mpecyna@ubuntu>
2025-09-15 17:25:25 +0200

    Plus que la loop

--- JeuHanoi.py
+++ JeuHanoi.py
@@ -1,12 +1,34 @@
 import numpy as np
 
+class Jeu_Hanoi:
+    def __init__(self):
+        # Initialisation de la matrice pic (3 tours avec 3 niveaux possibles)
+        self.pic = np.zeros([3, 3], dtype=np.int)  # Matrice 3x3 pour les tours et les palets
+        self.nombre_palet = np.zeros(3, dtype=np.int)  # Nombre de palets sur chaque tour
 
-class Jeu_Hanoi: 
-    def __init__(self): 
-        self.pic = np.zeros([3,3],dtype=np.int)
-        self.nombre_palet = np.zeros(3, dtype=np.int)
-        pass
-
-
+    def getPicValue(self, index):
+        # Retourne le nombre de palets sur la tour d'index donné
+        return self.nombre_palet[index]
         
-         
+    def getPics(self):
+        # Retourne la matrice pic qui contient les palets sur chaque tour
+        return self.pic
+
+    def getHighest(self, index):
+        for i in range(2, -1, -1):  # Parcours de la colonne de la tour spécifiée
+            if self.pic[index][i] != 0:  # Si un palet est trouvé (non nul)
+                return self.pic[index][i]  # Retourne la valeur du palet le plus haut
+        return 0  # Retourne 0 si aucun palet n'est trouvé sur la tour
+        
+    def addPalet(self, value, index):
+        self.pic[index][self.nombre_palet[index]] = value
+        self.nombre_palet[index] += 1  # Met à jour le nombre de palets sur la tour
+        
+    def deletePalet(self, index):
+        # Supprime le palet du sommet de la tour
+        self.pic[index][self.nombre_palet[index] - 1] = 0
+        self.nombre_palet[index] -= 1  # Diminue le nombre de palets sur la tour
+        
+    def printJeu(self):
+        print(self.pic)
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
 from JeuHanoi import Jeu_Hanoi
-
+from GameHandler import GameHandler
 jeu = Jeu_Hanoi() 
 
 #init de la partie au début
@@ -11,11 +11,44 @@ jeu.pic[0,1] = 2
 jeu.pic[0,2] = 1
 
 #init de la partie finale voulu => Condition d'arret 
-jeu_finale = Jeu_Hanoi()
+jeu_final = Jeu_Hanoi()
 
-Jeu_final.pic[2,0] = 3
-Jeu_final.pic[2,1] = 2
-Jeu_final.pic[2,2] = 1
+jeu_final.pic[2,0] = 3
+jeu_final.pic[2,1] = 2
+jeu_final.pic[2,2] = 1
 
+gh = GameHandler()
 
+turn = 0 
+#while gh.nombre_situation(jeu) != gh.nombre_situation(jeu_final):
+while(turn < 5):
+	
+    
+    
+    # Règle 1 : déplacer du pic 0 vers pic 1 si possible
+    if gh.regle_jeu(jeu, 0, 1) and gh.situation_non_vue(jeu, 0, 1):
+        print("REGLE 1 : Déplacement du pic 0 vers pic 1")
+        gh.effectue_deplacement(jeu, 0, 1)
+    
+    # Règle 2 : déplacer du pic 1 vers pic 2 si possible
+    elif gh.regle_jeu(jeu, 1, 2) and gh.situation_non_vue(jeu, 1, 2):
+        print("REGLE 2 : Déplacement du pic 1 vers pic 2")
+        
+        
+    # Règle 3 : déplacer du pic 2 vers pic 1 si possible
+    elif gh.regle_jeu(jeu, 2, 1) and gh.situation_non_vue(jeu, 2, 1):
+        print("REGLE 3 : Déplacement du pic 2 vers pic 1")
+        gh.effectue_deplacement(jeu, 2, 1)
+    
+    # Règle 4 : déplacer du pic 1 vers pic 0 si possible
+    elif gh.regle_jeu(jeu, 1, 0) and gh.situation_non_vue(jeu, 1, 0):
+        print("REGLE 4 : Déplacement du pic 1 vers pic 0")
+        gh.effectue_deplacement(jeu, 1, 0)
+    
+    else: 
+        print("Erreur")
+    
+    # Incrémentation du compteur de tours
+    turn += 1
 
+print("finit")
